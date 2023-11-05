dc88391111de4ea591062644ff53eafabf4f04fe
JigJug <easybot@tutanota.com>
2023-11-05 12:07:50 +0000

    test mongo

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,10 @@ const botController = new FatBotController();
 
 
 
-const uri = "mongodb+srv://easybot-alesam:alesam333@cluster0.uijr1x0.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_CONNECT//"mongodb+srv://easybot-alesam:alesam333@cluster0.uijr1x0.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(uri!, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
