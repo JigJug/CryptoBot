1cabad7e702a5657638b221528959e89d7eaa897
JigJug <easybot@tutanota.com>
2023-11-05 12:07:50 +0000

    test mongo

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const botController = new FatBotController();
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(uri!, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
